a801306792e4222fd544bef687ebef9158c1d93d
tumaa <ramanfatuu@gmail.com>
2019-10-26 15:25:42 +0300

    register  main  and auth blueprint for application

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,13 @@ def create_app(config_name):
     login_manager.init_app(app)
     # configure UploadSet
     
-    
+    configure_uploads(app,photos)
+    # Register main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    # Register auth blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     return app
 
 
