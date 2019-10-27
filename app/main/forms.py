3b23d85fa433bb40c4094061a83672eb8960cd03
tumaa <ramanfatuu@gmail.com>
2019-10-27 18:47:31 +0300

    defined forms for the user to be able to write  a blog and commment on it

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,24 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, SelectField
-from wtforms.validators import Required,Email
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
 
-
+class ReviewForm(FlaskForm):
+    title = StringField('Review title',validators=[Required()])
+    review = TextAreaField('Blog review', validators=[Required()])
+    submit = StringField('Submit')
 class UpdateProfile(FlaskForm):
-   bio = TextAreaField('Tell us something about you.',validators = [Required()])
-   submit = SubmitField('Submit')
-   
-class BlogForm(FlaskForm):
-   title = StringField('Blog Title',validators=[Required()])
-   blog_body = TextAreaField('Write Blog Content',validators=[Required()])
-   blog_category = SelectField('Blog Category',choices=[('Sports-Blog','Sports'),('Travel-Blog','Travel'),('Fitness-Blog','Fitness'),('Fashion-Blog','Fashion'),('Food-Blog','Food'),('Political-Blog','Politics')],validators=[Required()])
-   submit = SubmitField('Submit Blog')
-   
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+class CategoryForm(FlaskForm):
+    '''
+    Class to create a wtf form for creating a blog
+    '''
+    name =  StringField('Category Name', validators=[Required()])
+    submit = SubmitField('Create')
 class CommentForm(FlaskForm):
-   name = StringField('Enter Your Name',validators=[Required()])
-   comment = TextAreaField('Comments', validators=[Required()])
-   submit = SubmitField('Submit Comment')
-   
-class SubscribeForm(FlaskForm):
-   subscriber_name = StringField('Enter your Full Name',validators=[Required()])
-   subscriber_email = StringField('Enter your Email',validators=[Required(),Email()])
-   submit = SubmitField('Subscribe')
+    '''
+    Class to create a wtf form for creating a blog
+    '''
+    opinion = TextAreaField('WRITE COMMENT')
+    submit = SubmitField('SUBMIT')
+class BlogForm(FlaskForm):
+    '''
+    Class to create a wtf form for creating a pitch
+    '''
+    content = TextAreaField('YOUR BLOG')
+    submit = SubmitField('CREATE')
