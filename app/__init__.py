3f0aa7494d5224f2128fa32d8626f4f9f793dd37
tumaa <ramanfatuu@gmail.com>
2019-10-27 17:37:31 +0300

    initialised sending of emails inside our main init folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,42 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 login_manager = LoginManager()
-photos = UploadSet('photos',IMAGES)
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+mail = Mail()
+
 
 def create_app(config_name):
     app = Flask(__name__)
-    
-    # App Configurations
+
+    # creating app configurations
     app.config.from_object(config_options[config_name])
-    # Initialize flask extensions
+    app.config['SECRET_KEY'] = 'mzinge'
+
+    # configure UploadSet
+
+
+    # initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    # configure UploadSet
-    
-    configure_uploads(app,photos)
-    # Register main blueprint
+    mail.init_app(app)
+
+    # registering the Blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-    # Register auth blueprint
+
+    # registering the auth Blueprint
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+
     return app
-
-
-
